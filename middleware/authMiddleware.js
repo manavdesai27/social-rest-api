51206a25ab8789b41172c61b5e6ccd29313adb32
Manav Desai <manav27202@gmail.com>
2022-03-03 21:47:50 +0530

    middleware fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(' ')[1]
+  const refreshToken = req.headers["x-refresh-token"];
+  const token = authHeader && authHeader.split(" ")[1];
   // var token = req.headers["x-access-token"];
 
   if (token == null) return res.sendStatus(401);
