c664b6c7326f002ca75cf9d2c0656212bafd4316
Manav Desai <manav27202@gmail.com>
2022-03-03 22:31:56 +0530

    auth test

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  const token = req.headers["authorization"];
   // const refreshToken = req.headers["x-refresh-token"];
   // const token = authHeader && authHeader.split(" ")[1];
-  var token = req.headers["x-access-token"];
+  // var token = req.headers["x-access-token"];
 
   if (token == null) return res.sendStatus(401);
   console.log(token);
